ddd67f61473c6ccad3d149c399a0672c58a4f0b1
MarkoGodra <marko.godra@gmail.com>
2017-06-06 12:15:08 +0200

    Added vector is empty check

--- src/reciever.cpp
+++ src/reciever.cpp
@@ -28,11 +28,14 @@ std::mutex common_buffer_mutex;
 std::mutex file_mutex;
 //char *path = "/home/godra/Desktop/example.png";
 
-unsigned char dest_mac_eth[6] = { 0x10, 0x1f, 0x74, 0xcc, 0x28, 0xf9};
-unsigned char src_mac_eth[6] = { 0x40, 0x16, 0x7e, 0x84, 0xb9, 0x8a};
+unsigned char dest_mac_eth[6] = { 0x10, 0x1f, 0x74, 0xcc, 0x28, 0xf9}; //steva
+unsigned char src_mac_eth[6] = { 0x40, 0x16, 0x7e, 0x84, 0xb9, 0x8a}; //godra
 
 unsigned char src_ip_eth[4] = {0x0a, 0x51, 0x23, 0x29};
 unsigned char dest_ip_eth[4] = {0x0a, 0x51, 0x23, 0x2b};
+
+unsigned char dest_mac_wlan[6] = {0x60, 0xd8, 0x19, 0x59, 0x0d, 0xb3}; //steva
+unsigned char src_mac_wlan[6] = {0x54, 0x27, 0x1e, 0x83, 0x59, 0x8d}; //godra
 
 int main(){
 
@@ -168,7 +171,9 @@ int main(){
     }
 
     std::thread eth_thread(eth_thread_function, device_handle_eth);
+    std::thread wlan_thread(wlan_thread_function, device_handle_wlan);
     eth_thread.join();
+    wlan_thread.join();
 
     //file.close();
     return 0;
@@ -224,113 +229,218 @@ void eth_thread_function(pcap_t* device_handle){
 
     while((result = pcap_next_ex(device_handle, &packet_header, &packet_data)) >= 0){
 
+        //Allocating new buffer for acepting data
         char* temp = new char[DATA_SIZE];
 
 		if(result == 0) {
 			std::cout << "Timeout expired" << std::endl;
 		
-		}
-			else {
-			std::cout << result << std::endl;
+		}else {
 
-		   frame* pFrame;
-		   printf("Frame captured\n");
+            frame* pFrame;
+            pFrame = (frame*)packet_data;
 
-		   pFrame = (frame*)packet_data;
+            std::cout << "ETHERNET" << std::endl;
+            std::cout << "Result : " << result << std::endl;
+            std::cout << "Frame captured" << std::endl;
+            std::cout << "Expected :" << expected << std::endl;
+            std::cout << "Recieved : " << pFrame->fch.frame_count << std::endl;
+            std::cout << "Total data : " << pFrame->fch.num_of_total_frames << std::endl;
 
-		   std::cout << "Frame count of current frame: " << pFrame->fch.frame_count << "\n";
+            ack_frame af;
+            fill_ack_frame(&af, src_mac_eth, dest_mac_eth, pFrame->fch.frame_count, src_ip_eth, dest_ip_eth);
+            pcap_sendpacket(device_handle, (const unsigned char*)&af, sizeof(ack_frame));
 
-		   std::cout << "Expected : " << expected << "\n";
+            if(pFrame->fch.frame_count == expected){ //If frame is in order
+                //Lock here, for file manipulation
+                file_mutex.lock();
+                file.open("/home/godra/Desktop/example.png", std::ios::out | std::ios::app | std::ios::binary);
+                if(!file.is_open()){
+                    printf("File opening failed\n");
+                }
 
-		   std::cout << "Total frames : " << pFrame->fch.num_of_total_frames << "\n";
+                memcpy(temp, pFrame->fch.data, pFrame->fch.data_len * sizeof(char));
+                file.write(temp, pFrame->fch.data_len);
+                file.close();
+                expected = expected + 1; //Increment to next frame
+                file_mutex.unlock();
 
-			ack_frame af;
-		   fill_ack_frame(&af, src_mac_eth, dest_mac_eth, pFrame->fch.frame_count, src_ip_eth, dest_ip_eth);
-		   pcap_sendpacket(device_handle, (const unsigned char*)&af, sizeof(ack_frame));
+                //After writing to file check the out-of-order-buffer for more frames to write to file
+                std::vector<fc_header>::iterator it;
+                int i;
 
-		   if(pFrame->fch.frame_count == expected){ //If frame is in order
+                if(!common_buffer.empty()){
 
-		       std::cout << "Usao ovde\n";
-		       //Lock here, for file manipulation
-		       file_mutex.lock();
-               file.open("/home/godra/Desktop/example.png", std::ios::out | std::ios::app | std::ios::binary);
-		       if(!file.is_open()){
-		           printf("File opening failed\n");
-		       }
+                    //Lock out-of-order-buffer-mutex for walk through that buffer
+                    common_buffer_mutex.lock();
+                    it = common_buffer.begin();
+                    i = 0;
+                    while(it != common_buffer.end()) {
 
-               memcpy(temp, pFrame->fch.data, pFrame->fch.data_len * sizeof(char));
-               file.write(temp, pFrame->fch.data_len);
-                //file << pFrame->fch.data;
-		       file.close();
-		       expected = expected + 1; //Increment to next frame
-		       file_mutex.unlock();
+                        fc_header current_item = (fc_header)(*it);
 
-		       //After writing to file check the out-of-order-buffer for more frames to write to file
-		       std::vector<fc_header>::iterator it;
-		       int i;
+                        if(current_item.frame_count == expected){ //If the expected frame is found in out-of-order-buffer
 
-		       //Lock out-of-order-buffer-mutex for walk through that buffer
-		       common_buffer_mutex.lock();
-			   it = common_buffer.begin();
-			   i = 0;
-		       //for(it = common_buffer.begin(); it != common_buffer.end(); it++, i++){
-			   while(it != common_buffer.end()) {
+                            file_mutex.lock(); //Lock file mutex before writing to file
+                            file.open("/home/godra/Desktop/example.png"); //Open file
+                            if(!file.is_open()){
+                                printf("File opening failed");
+                            }
 
-					fc_header current_item = (fc_header)(*it);
+                            memcpy(temp, pFrame->fch.data, pFrame->fch.data_len * sizeof(char));
+                            file.write(temp, pFrame->fch.data_len);
+                            file.close(); //Close file
+                            expected = expected + 1; //Increment to expect next fram
+                            file_mutex.unlock(); //Unlock file mutex after writing to file
 
-					if(current_item.frame_count == expected){ //If the expected frame is found in out-of-order-buffer
-						//Lock file mutex before writing to file
-						file_mutex.lock();
-                        file.open("/home/godra/Desktop/example.png"); //Open file
-						if(!file.is_open()){
-							printf("File opening failed");
-						}
+                            common_buffer.erase(common_buffer.begin() + i); //Erase that good frame from out-of-order-buffer
+                            it = common_buffer.begin();
+                            i = 0;
+                        }
+                    i++;
+                    it++;
+                    }
 
-                        memcpy(temp, pFrame->fch.data, pFrame->fch.data_len * sizeof(char));
-                        file.write(temp, pFrame->fch.data_len);
+                    //Unlock out-of-order-buffer-mutex after iteration through it
+                    common_buffer_mutex.unlock();
+                }
 
-                        //file << current_item.data; //Write it to file
-						file.close(); //Close file
-						expected = expected + 1; //Increment to expect next fram
-						//Unlock file mutex after writing to file
-						file_mutex.unlock();
+           } else if (strcmp(((const char *)pFrame->eh.src_address), (const char *)src_mac_eth) == 0) {
 
-						common_buffer.erase(common_buffer.begin() + i); //Erase that good frame from out-of-order-buffer
-						it = common_buffer.begin();
-						i = 0;
-					}
-					i++;
-					it++;
-		       }
+                std::cout << "\nIde preko reda!!!\n";
 
-		       //Unlock out-of-order-buffer-mutex after iteration through it
-		       common_buffer_mutex.unlock();
+                common_buffer_mutex.lock(); //Lock down here for adding it to temp buffer (for out of order frames)
 
-		   } else {
+                common_buffer.push_back(pFrame->fch);
+                std::vector<fc_header>::iterator it;
+                std::cout << "###########Sadrzaj bafera za out of order ###########" << std::endl;
+                for(it = common_buffer.begin(); it != common_buffer.end(); it++){
+                    fc_header current_item = (fc_header)(*it);
+                    std::cout << current_item.num_of_total_frames << std::endl;
+                }
+                std::cout << "#####################################################" << std::endl;
 
-		       std::cout << "\nIde preko reda!!!\n";
-
-		       //Lock down here for adding it to temp buffer (for out of order frames)
-		       common_buffer_mutex.lock();
-		       common_buffer.push_back(pFrame->fch);
-		       //Unlock after adding it to temp buffer
-				std::vector<fc_header>::iterator it;
-				std::cout << "###########Sadrzaj bafera za out of order ###########" << std::endl;
-				for(it = common_buffer.begin(); it != common_buffer.end(); it++){
-					fc_header current_item = (fc_header)(*it);
-					std::cout << current_item.num_of_total_frames << std::endl;
-				}
-				std::cout << "#####################################################" << std::endl;			
-
-		       common_buffer_mutex.unlock();
+                common_buffer_mutex.unlock(); //Unlock after adding it to temp buffer
 
 		   }
 
            delete[] temp;
 
-		   //printf("Recieving data %.2f '%' \r", (float)pFrame->fch.frame_count / pFrame->fch.num_of_total_frames * 100);
 		   if(pFrame->fch.num_of_total_frames == expected)
 		        break;
 		}
+    }
+}
+
+void wlan_thread_function(pcap_t* device_handle){
+
+    int result;							// result of pcap_next_ex function
+    struct pcap_pkthdr* packet_header;	// header of packet (timestamp and length)
+    const unsigned char* packet_data;	// packet content
+
+
+    printf("\nRecieveing data over ethernet\n");
+
+    while((result = pcap_next_ex(device_handle, &packet_header, &packet_data)) >= 0){
+
+        //Allocating new buffer for acepting data
+        char* temp = new char[DATA_SIZE];
+
+        if(result == 0) {
+            std::cout << "Timeout expired" << std::endl;
+        }else {
+
+            frame* pFrame;
+            pFrame = (frame*)packet_data;
+
+            std::cout << "WLAN" << std::endl;
+            std::cout << "Result : " << result << std::endl;
+            std::cout << "Frame captured" << std::endl;
+            std::cout << "Expected :" << expected << std::endl;
+            std::cout << "Recieved : " << pFrame->fch.frame_count << std::endl;
+            std::cout << "Total data : " << pFrame->fch.num_of_total_frames << std::endl;
+
+            ack_frame af;
+            fill_ack_frame(&af, src_mac_wlan, dest_mac_wlan, pFrame->fch.frame_count, src_ip_eth, dest_ip_eth);
+            pcap_sendpacket(device_handle, (const unsigned char*)&af, sizeof(ack_frame));
+
+            if(pFrame->fch.frame_count == expected){ //If frame is in order
+                //Lock here, for file manipulation
+                file_mutex.lock();
+                file.open("/home/godra/Desktop/example.png", std::ios::out | std::ios::app | std::ios::binary);
+                if(!file.is_open()){
+                    printf("File opening failed\n");
+                }
+
+                memcpy(temp, pFrame->fch.data, pFrame->fch.data_len * sizeof(char));
+                file.write(temp, pFrame->fch.data_len);
+                file.close();
+                expected = expected + 1; //Increment to next frame
+                file_mutex.unlock();
+
+                //After writing to file check the out-of-order-buffer for more frames to write to file
+                std::vector<fc_header>::iterator it;
+                int i;
+
+                if(common_buffer.emplace) {
+
+                    //Lock out-of-order-buffer-mutex for walk through that buffer
+                    common_buffer_mutex.lock();
+                    it = common_buffer.begin();
+                    i = 0;
+                    while(it != common_buffer.end()) {
+
+                        fc_header current_item = (fc_header)(*it);
+
+                        if(current_item.frame_count == expected){ //If the expected frame is found in out-of-order-buffer
+
+                            file_mutex.lock(); //Lock file mutex before writing to file
+                            file.open("/home/godra/Desktop/example.png"); //Open file
+                            if(!file.is_open()){
+                                printf("File opening failed");
+                            }
+
+                            memcpy(temp, pFrame->fch.data, pFrame->fch.data_len * sizeof(char));
+                            file.write(temp, pFrame->fch.data_len);
+                            file.close(); //Close file
+                            expected = expected + 1; //Increment to expect next fram
+                            file_mutex.unlock(); //Unlock file mutex after writing to file
+
+                            common_buffer.erase(common_buffer.begin() + i); //Erase that good frame from out-of-order-buffer
+                            it = common_buffer.begin();
+                            i = 0;
+                        }
+                        i++;
+                        it++;
+                    }
+
+                    //Unlock out-of-order-buffer-mutex after iteration through it
+                    common_buffer_mutex.unlock();
+                }
+
+           } else if (strcmp(((const char *)pFrame->eh.src_address), (const char *)src_mac_eth) == 0) {
+
+                std::cout << "\nIde preko reda!!!\n";
+
+                common_buffer_mutex.lock(); //Lock down here for adding it to temp buffer (for out of order frames)
+
+                common_buffer.push_back(pFrame->fch);
+                std::vector<fc_header>::iterator it;
+                std::cout << "###########Sadrzaj bafera za out of order ###########" << std::endl;
+                for(it = common_buffer.begin(); it != common_buffer.end(); it++){
+                    fc_header current_item = (fc_header)(*it);
+                    std::cout << current_item.num_of_total_frames << std::endl;
+                }
+                std::cout << "#####################################################" << std::endl;
+
+                common_buffer_mutex.unlock(); //Unlock after adding it to temp buffer
+
+           }
+
+           delete[] temp;
+
+           if(pFrame->fch.num_of_total_frames == expected)
+                break;
+        }
     }
 }
